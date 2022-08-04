0adf3fafb3004432441435c2d96e30e075e5e6fb
Asadullokh Nurullaev <87550552+asadullokhn@users.noreply.github.com>
2022-08-05 01:08:51 +0500

    update

--- src/data/projects-list.ts
+++ src/data/projects-list.ts
@@ -1,58 +1,53 @@
-import peterpitch from "assets/images/projects/pp.png";
+import dgLogo from "assets/images/projects/dg_logo.png";
 import notch from "assets/images/projects/notch.png";
 import workof from "assets/images/projects/workof.png";
 import souq from "assets/images/projects/souq.png";
-import gosave from "assets/images/projects/gosave.png";
+import botLogo from "assets/images/projects/bot.png";
 
 export const projectsList = [
   {
     id: 1,
     title: "DigitalGeneration Uzbekistan",
-    logo: peterpitch,
+    logo: dgLogo,
     blurHash: "L4ADc400P*Zi4Tu1y;Qo00pH#YXl",
     link: "https://new.raqamliavlod.uz/",
-    desc: "This platform ",
-    technologies: ["WPF", "Asp.Net"]
+    desc: "This is a platform which has courses and forums where each user can learn and ask questions in any IT sphere for free. Besides that, this platform also has checker system, which helps to users to improve algorithmic performance by solving problems",
+    technologies: ["Asp.Net", "WPF"]
   },
   {
     id: 2,
-    title: "Notch",
+    title: "Qualification",
     link: "https://www.hellonotch.com/",
     logo: notch,
     blurHash: "L0Aer?tjH[tPyAayj[j[00ay%xkB",
-    desc: `This is a project management website for design and procurement teams. 
-    It helps to collaborate and manage all of your floor plans in one place, writing specs, budgeting and timelines. 
-    This project management website provides a wide variety of features like timeline tracking, spec sheet generation, integrated budget, vendor + inventory management, user usage tracking and many more.`,
-    technologies: ["ruby", "rails", "javascript"]
+    desc: "Currently developing web backend for maktab.uz (Ministry of Education), which focus on testing students and collecting statistics of all schools and student of whole country.",
+    technologies: ["Asp.Net", "C#"]
   },
   {
     id: 3,
-    title: "Workof Ecommerce",
+    title: "POS system",
     logo: workof,
     blurHash: "L2M5%%0000EN00I:-oIp00kC?^sl",
     link: "https://workof.com/",
-    desc: `This is an Ecommerce furniture site. It provides a single platform to multiple vendors where they can put their products on sale.`,
-    technologies: ["ruby", "rails", "javascript"]
+    desc: "Successfully completed point of sale platform on WPF. A material design has been used to achieve a great user interface.",
+    technologies: ["WPF", "C# | XAML"]
   },
   {
     id: 4,
-    title: "Souq of Qatar",
+    title: "Test-Mixer",
     logo: souq,
     blurHash: "L1O^uE0679I00AGT?.H{00y7p2tK",
-    link: "https://www.souqofqatar.com/",
-    desc: `Souq of Qatar is online comparison website. This website is independent of any retailers, it displays the products of all the major stores.
-    Just like when you compare prices and products while visiting physical shops and stores, online price comparison behaves in the same way. 
-    It is an established method in the West for people to visit the online comparison websites first whenever they are in need to buy something. 
-    This provides the benefits of convenience, choice, and price efficiency.`,
-    technologies: ["ruby", "rails", "javascript"]
+    link: null,
+    desc: "A platform for preparing unique tests for each student of the presidential school (Muhammad al-Khwarizmi Specialized IT School) has been successfully developed.",
+    technologies: []
   },
   {
     id: 5,
-    title: "GoSave",
-    logo: gosave,
+    title: "TextagramBot",
+    logo: botLogo,
     blurHash: "L09sWSxdH;s?%6axWAfk00V?%jax",
-    link: "https://gosavehq.com/",
-    desc: `The GoSave app let's you send allowances, assign chores, and approve spending all from your smartphone. These actions appear on each kid's toy, along with a live bank balance so they can watch their savings grow.`,
-    technologies: ["ruby", "rails"]
+    link: "https://t.me/Shrifty_textprobot",
+    desc: "The bot, which has over 50,000 users, converts English and Russian text into creative fonts",
+    technologies: ["Aiogram", "Python"]
   }
 ];
